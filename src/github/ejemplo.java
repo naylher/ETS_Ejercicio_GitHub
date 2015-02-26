9e23c15903f0942c03af789fd9e36631e1b2ab82
UlyssesOrtega <UlyssesOrtega@192.168.1.11>
2015-02-26 20:55:07 +0000

    Actualización del repositorio, v.1.1

--- src/github/ejemplo.java
+++ src/github/ejemplo.java
@@ -5,7 +5,8 @@ public class ejemplo {
 	public static void main(String[] args) {
 		
 		System.out.print("Esta es la version 1.0 de este ejercicio");
-
+		
+		System.out.print("Agrago esta segunda linea en la version 1.1");
 	}
 
 }
