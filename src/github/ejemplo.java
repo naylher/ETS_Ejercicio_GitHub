8b538aa22bcf085f62743e499ff09b49ba91caaf
Ulysses Ortega <naylher@hotmail.com>
2015-02-27 10:50:23 +0000

    Update ejemplo.java
    soy el remote y vengo a destrozarte la maquina

--- src/github/ejemplo.java
+++ src/github/ejemplo.java
@@ -4,7 +4,7 @@ public class ejemplo {
 
 	public static void main(String[] args) {
 		
-		System.out.print("Esta es la version 1.0 de este ejercicio");
+		System.out.print("Esta es la version 1.5 de este ejercicio");
 		
 		System.out.print("Agrago esta segunda linea en la version 1.1");
 	}
